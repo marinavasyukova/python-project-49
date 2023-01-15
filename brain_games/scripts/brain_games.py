dfa6262c0ef2932021546984522ae2732793fdad
Marina Vasyukova <marina.vasyukova@icloud.com>
2023-01-16 01:29:01 +0300

    Add the third step of project

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
 
-def main():
-    print('Welcome to the Brain Games!')
-
+def main():  
+    print('Welcome to the brain Games!')
+    welcome_user()
 
 if __name__ == '__main__':
     main()
